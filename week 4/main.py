35e751fc6c664526477a51934dc49ae72d7c99c4
Anurup Paudel <anurupbro@gmail.com>
2023-06-20 18:54:18 -0700

    working on dictionary items.

---
+++ week 4/main.py
@@ -0,0 +1,25 @@
+
+# creates a dictionary with food prices.
+food_prices = {
+    "chicken" : 1.59,
+    "beef" : 1.99,
+    "cheese" : 1.00,
+    "milk" : 2.50, 
+}
+
+# assigned a variable for each food price.
+chicken_price = food_prices["chicken"]
+beef_price = food_prices["beef"]
+cheese_price = food_prices["cheese"]
+milk_price = food_prices["milk"]
+
+# prints the price for food items from the variable it was assigned to.
+print(chicken_price)
+print(beef_price)
+print(cheese_price)
+print(milk_price)
+
+# alternatively if we were to print the price with the item name 
+# prints the dictionary with all its contents.
+print(food_prices)
+
